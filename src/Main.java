3a04363fc7c0b4b1517d2fecfc7ce3cc7df384ef
Andrei Shishkin <shishkinam82@gmail.com>
2021-05-16 23:22:50 +0400

    add code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5000;
+        int price1mile = 20;
+        int totalMile = ticketPrice / price1mile;
+        System.out.println(totalMile);
+    }
+}
